030ae3c1b91ca50d52a59094960756699d938d81
Xavier CARL <xavier.carl@soprasteria.com>
2020-08-19 09:54:47 +0200

    config nb thread

--- src/main/java/demo/notification/middle/configuration/WebSocketConfig.java
+++ src/main/java/demo/notification/middle/configuration/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package demo.notification.middle.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -10,6 +11,11 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+	@Override
+	public void configureClientOutboundChannel(ChannelRegistration registration) {
+		registration.taskExecutor()
+				.corePoolSize(20);
+	}
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
