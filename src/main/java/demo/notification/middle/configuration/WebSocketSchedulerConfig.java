16ab3688fae220389cc836ce9a43dbf4277a2e7c
Xavier CARL <xavier.carl@soprasteria.com>
2020-08-19 09:55:11 +0200

    scheduler de publication de couleurs

--- src/main/java/demo/notification/middle/configuration/WebSocketSchedulerConfig.java
+++ src/main/java/demo/notification/middle/configuration/WebSocketSchedulerConfig.java
@@ -1,44 +1,35 @@
 package demo.notification.middle.configuration;
 
-import demo.notification.middle.dto.MessageNotification;
+import demo.notification.middle.bll.MatriceCouleurBlo;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatterBuilder;
-import java.time.format.SignStyle;
-
-import static java.time.temporal.ChronoField.*;
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 @Configuration
 @EnableScheduling
+@Log4j2
 public class WebSocketSchedulerConfig {
 
 	@Autowired
 	private SimpMessagingTemplate template;
 
-//	@Scheduled(fixedRate = 3000)
+	@Resource
+	private MatriceCouleurBlo matriceCouleurBlo;
+
+//	@Scheduled(fixedRate = 5000)
 	public void publishUpdates() {
-		String message = "It's " + LocalDateTime.now().format(new DateTimeFormatterBuilder()
-				.appendValue(HOUR_OF_DAY, 2)
-				.appendLiteral(':')
-				.appendValue(MINUTE_OF_HOUR, 2)
-				.appendLiteral(':')
-				.appendValue(SECOND_OF_MINUTE, 2)
-				.appendLiteral(" - ")
-				.appendValue(DAY_OF_MONTH, 2)
-				.appendLiteral('/')
-				.appendValue(MONTH_OF_YEAR, 2)
-				.appendLiteral('/')
-				.appendValue(YEAR, 4, 10, SignStyle.EXCEEDS_PAD)
-				.toFormatter());
-		MessageNotification messageNotification = new MessageNotification();
-		messageNotification.setContenu(message);
-		messageNotification.setAuthor("System");
-		System.out.println("Message: " + message);
-		template.convertAndSend("/topic/notifications", messageNotification);
+		List<String> givenList = Arrays.asList("red", "blue", "yellow", "green", "black", "purple");
+		Random rand = new Random();
+		String randomElement = givenList.get(rand.nextInt(givenList.size()));
+
+		matriceCouleurBlo.activerMatriceCouleur(randomElement, 5, 100);
 	}
 
 }
